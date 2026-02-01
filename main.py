62111a17b3fb4ff48b4dae20ab95b8ff02b013bd
davevers <daverstrate@gmail.com>
2026-02-01 13:57:36 +0100

    add user prompt in argument

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 from google import genai
-from google.genai.types import UsageMetadata
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-if not api_key:
-    raise RuntimeError("No api key found")
 
-client = genai.Client(api_key=api_key)
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-print(f"User prompt: {prompt}")
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", contents=prompt
-)
-if not response.usage_metadata:
-    raise RuntimeError("API request has failed")
-print(
-    f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
-)
-print(f"Response:\n{response.text}")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("No api key found")
+    client = genai.Client(api_key=api_key)
+
+    print(f"User prompt: {args.user_prompt}")
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", contents=args.user_prompt
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("API request has failed")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
