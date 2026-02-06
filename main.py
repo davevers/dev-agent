95c2ce1c661e1b010a3be64a6951a5a9ed9dc1ba
davevers <daverstrate@gmail.com>
2026-02-06 13:13:13 +0100

    add multiple messages and verbose output

--- main.py
+++ main.py
@@ -3,27 +3,38 @@ import os
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
-        raise RuntimeError("No api key found")
+        raise RuntimeError("GEMINI_API_KEY environment variable not found")
     client = genai.Client(api_key=api_key)
 
-    print(f"User prompt: {args.user_prompt}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
-        model="gemini-3-flash-preview", contents=args.user_prompt
+        model="gemini-3-flash-preview", contents=messages
     )
+
     if not response.usage_metadata:
         raise RuntimeError("API request has failed")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
